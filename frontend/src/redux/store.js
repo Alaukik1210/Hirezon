6a08756fcbf168c558f3ebe62bc9291d483cc3f0
Alaukik Miishra <151136125+Alaukik1210@users.noreply.github.com>
2025-01-24 17:35:13 +0530

    jobSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authSlice from "./authSlice"
+import jobSlice from './jobSlice'
 
 const store = configureStore({
     reducer:{
-        auth:authSlice
+        auth:authSlice,
+        job:jobSlice
     },
     /* preloadedState, */
 devTools:     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
